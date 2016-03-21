4e4045b4d536bc6d76acf50140859f54ba4ad2ef
baku89 <mail@baku89.com>
2016-03-22 08:45:47 +0900

    Add ofxDatGui assets

--- of/src/ofApp.cpp
+++ of/src/ofApp.cpp
@@ -1,7 +1,24 @@
 #include "ofApp.h"
 
+#define GUI_WIDTH 200
+
 //--------------------------------------------------------------
 void ofApp::setup(){
+	
+	ofSetWindowTitle("OfficeBlink");
+	ofSetWindowShape(640 + GUI_WIDTH, 160);
+	ofSetFrameRate(60);
+	
+	syphonClient.setup();
+	syphonClient.set(SYPHON_NAME, SYPHON_APP);
+	
+	
+	gui = new ofxDatGui(ofxDatGuiAnchor::TOP_LEFT);
+//	gui->setTheme(new ofxDatGuiThemeSmoke());
+	gui->setWidth(GUI_WIDTH);
+	gui->addHeader();
+	gui->addTextInput("port")->setText("8989");
+	gui->addToggle("start serving");
 
 }
 
@@ -12,7 +29,18 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+	
+	int width = ofGetWidth() - GUI_WIDTH;
+	int height = ofGetHeight();
+	
+	ofClear(0);
+	
+	ofPushMatrix();
+	ofTranslate(GUI_WIDTH, 0);
+	
+	syphonClient.draw(0, 0);
+	
+	ofPopMatrix();
 }
 
 //--------------------------------------------------------------
